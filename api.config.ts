82dc832d7ee7f52a22aee810c368b60b79472d56
xiaoyao-Ye <yqcyecong@163.com>
2023-03-05 20:18:25 +0800

    test: Add test json

--- api.config.ts
+++ api.config.ts
@@ -4,6 +4,8 @@ export default defineConfig({
   importAxios: 'import axios from "../index";',
   useAxios: 'axios.request',
   swagger: {
+    // TODO test: swagger2openapi 转换为 openapi3 时会有问题，所以出现 UserArray 的class
+    test: 'https://petstore.swagger.io/v2/swagger.json',
     mangosteen: 'http://127.0.0.1:3000/api-json',
   },
   outputDir: './src/api',
