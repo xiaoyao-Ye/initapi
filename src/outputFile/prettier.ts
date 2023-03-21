1ec58516377865e24df5f36e30dd4989b8dae1bf
xiaoyao-Ye <yqcyecong@163.com>
2023-03-21 11:44:12 +0800

    feat: Allow custom .prettier configuration

--- src/outputFile/prettier.ts
+++ src/outputFile/prettier.ts
@@ -1,22 +1,46 @@
 import prettier from 'prettier'
+import path from 'path'
+import { tryRequire } from '../utils/index'
 
 const defaultOptions: prettier.Options = {
   // 解析器
   parser: 'typescript',
-  // 解析规则
-  // 单引号
-  singleQuote: false,
-  // 尾分号
+  // 一行最多 100 字符
+  printWidth: 100,
+  // 使用 2 个空格缩进
+  tabWidth: 2,
+  // 不使用缩进符使用空格
+  useTabs: false,
+  // 行尾需要有分号
   semi: true,
+  // 使用单引号
+  singleQuote: true,
+  // 对象的 key 仅在必要时用引号
+  quoteProps: 'as-needed',
+  // jsx 不使用单引号，而使用双引号
   jsxSingleQuote: false,
-  trailingComma: 'none',
+  // 尾随逗号
+  trailingComma: 'all',
+  // 大括号内的首尾需要空格
   bracketSpacing: true,
   bracketSameLine: false,
+  // jsx 标签的反尖括号需要换行
+  jsxBracketSameLine: false,
+  // 箭头函数，只有一个参数的时候，也需要括号always, avoid
+  arrowParens: 'avoid',
+  // 每个文件格式化的范围是文件的全部内容
   rangeStart: 0,
   rangeEnd: Infinity,
+  // 不需要写文件开头的 @prettier
+  requirePragma: false,
+  // 不需要自动在文件开头插入 @prettier
+  insertPragma: false,
+  // 使用默认的折行标准
   proseWrap: 'preserve',
-  arrowParens: 'avoid',
-  endOfLine: 'lf',
+  // 根据显示样式决定 html 要不要折行
+  htmlWhitespaceSensitivity: 'css',
+  // 换行符使用 auto, lf
+  endOfLine: 'auto',
 }
 
 /**
@@ -25,6 +49,15 @@ const defaultOptions: prettier.Options = {
  * @param options 格式化规则
  * @returns 格式化后的数据
  */
-export default function format(code: string, options: prettier.Options = defaultOptions): string {
-  return prettier.format(code, options)
+export default async function format(code: string): Promise<string> {
+  const prettierConfig = await getOptions();
+  return prettier.format(code, prettierConfig)
+}
+
+/**
+ * 获取当前node运行目录下的 .prettier 文件的配置进行格式化
+ */
+async function getOptions(): Promise<prettier.Options> {
+  const options = await tryRequire(path.join(process.cwd(), '.prettierrc'))
+  return { ...defaultOptions, ...options }
 }
