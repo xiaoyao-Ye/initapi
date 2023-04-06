ee299583dfa037afd44b954ac480f7fa116e817e
xiaoyao-Ye <yqcyecong@163.com>
2023-04-06 21:47:44 +0800

    feat(config): definition

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -8,24 +8,26 @@ export type UserConfigFn = (env: UserConfig) => UserConfig | Promise<UserConfig>
 export interface ConfigEnv {}
 
 export interface UserConfig {
-  /** 导入请求函数 */
+  /** 导入请求函数 default: 'import axios from "axios";' */
   importAxios?: string
-  /** 使用请求函数 */
+  /** 使用请求函数 default: 'axios.request' */
   useAxios?: string
-  /** api服务对应的swagger/openapi地址 */
+  /** api服务对应的swagger/openapi地址 default: { test: 'https://petstore.swagger.io/v2/swagger.json' } */
   swagger?: { [prop: string]: string }
   /** TS entity的属性是否可空, false根据文档生成, true 会加上? */
-  nullable?: boolean
-  /** 生成文件存放位置-目录 */
+  // nullable?: boolean
+  /** 生成文件存放位置 default: './api' */
   outputDir?: string
   /** 生成文件类型-无配置时会弹出命令行交互让用户选择 */
   outputType?: 'TypeScript' | 'JavaScript'
+  /** 类型定义 default: interface (两者的区别请自行查询TypeScript) */
+  definition?: 'class' | 'interface'
   /** entity class construct */
   /** api 返回类型默认值 Object.assign 或 new class(有一个问题是需要分辨是enum还是class) 理论上后端或者前端自行处理比较合理, api处理作用将不再单一 */
 }
 
 /**
- * Type helper to make it easier to use vite.config.ts
+ * Type helper to make it easier to use api.config.ts
  * accepts a direct {@link UserConfig} object, or a function that returns it.
  * The function receives a {@link ConfigEnv} object that exposes two properties:
  */
@@ -33,6 +35,7 @@ export function defineConfig(config: UserConfigExport): UserConfigExport {
   return config
 }
 
+/** 默认配置 */
 const defaultOptions = () => ({
   importAxios: 'import axios from "axios";',
   useAxios: 'axios.request',
@@ -40,6 +43,7 @@ const defaultOptions = () => ({
     test: 'https://petstore.swagger.io/v2/swagger.json',
   },
   outputDir: './api',
+  definition: 'interface'
 })
 
 export const getConfig = async (): Promise<UserConfig> => {
