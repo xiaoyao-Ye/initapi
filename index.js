5285bcde2c33480fed0efaa94ae61c1c100f2b1f
xiaoyao-Ye <yqcyecong@163.com>
2023-04-07 03:19:20 +0800

    chore: build

---
+++ index.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const path = require('path')
+
+const sourceFiles = [
+  ['./package.json', './lib/package.json'],
+  ['./CHANGELOG.md', './lib/CHANGELOG.md'],
+  ['./README.md', './lib/README.md'],
+  ['./src/template/typeScript/api.ejs', './lib/template/typeScript/api.ejs'],
+  ['./src/template/typeScript/typings.d.ejs', './lib/template/typeScript/typings.d.ejs'],
+  ['./src/template/javaScript/api.ejs', './lib/template/javaScript/api.ejs'],
+]
+
+fs.mkdirSync(path.join(__dirname, './lib/template/typeScript'), { recursive: true }, (err) => {
+  if (err) throw err
+  console.log('Directory created successfully')
+})
+fs.mkdirSync(path.join(__dirname, './lib/template/javaScript'), { recursive: true }, (err) => {
+  if (err) throw err
+  console.log('Directory created successfully')
+})
+
+for (let [sourcePath, targetPath] of sourceFiles) {
+  fs.copyFile(sourcePath, targetPath, (err) => {
+    if (err) throw err
+    console.log(`${sourcePath} was copied to ${targetPath}`)
+  })
+}
