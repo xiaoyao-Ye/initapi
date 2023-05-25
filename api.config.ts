1bbc64b574cb8270a14335ae376737e2e8ec59fe
Ghosteye <yqcyecong@163.com>
2023-05-26 00:32:49 +0800

    test: Add todo and test

--- api.config.ts
+++ api.config.ts
@@ -7,6 +7,9 @@ export default defineConfig({
     // TODO test: swagger2openapi 转换为 openapi3 时会有问题，所以出现 UserArray 的class
     test: 'https://petstore.swagger.io/v2/swagger.json',
     mangosteen: 'http://127.0.0.1:3000/api-json',
+    test1: './test/swagger.json',
+    test2: './test/swagger2.json',
+    huanqiu: './test/openapi.3.0.json',
   },
   outputDir: './src/api',
   // outputType: 'TypeScript',
