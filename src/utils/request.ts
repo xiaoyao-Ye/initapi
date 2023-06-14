8a50f9a8362493c6d4d1bb8b35aff560243ba489
Ghosteye <yqcyecong@163.com>
2023-06-14 15:48:59 +0800

    feat: Change transform method

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,6 +1,7 @@
-import converter from "swagger2openapi";
+// import converter from "swagger2openapi";
 import openapiParser from "./openapi-parser.mjs";
-import type { OpenAPIObject } from "openapi3-ts/dist/mjs";
+// import type { OpenAPIObject } from "openapi3-ts/dist/mjs";
+import { OpenAPIObject } from "openapi3-ts/oas31";
 import { isHttp, readJson } from "./index";
 import axios from "axios";
 
@@ -23,7 +24,14 @@ export const getInitData = async (url: string): Promise<OpenAPIObject> => {
 };
 
 const transformOpenApi = async (data: OpenAPIObject): Promise<OpenAPIObject> => {
-  const { openapi } = await (converter as any).convertObj(data, { patch: true, warnOnly: true });
-  const { openapi } = openapiParser.convertObj(openapi);
-  return openapi;
+  // const { openapi } = await (converter as any).convertObj(data, { patch: true, warnOnly: true });
+  const { openapi } = openapiParser.convertObj(data);
+  return openapi as OpenAPIObject;
+
+  // return new Promise(resolve => {
+  //   import("./openapi-parser.mjs").then(({ default: { convertObj } }) => {
+  //     const { openapi } = convertObj(data);
+  //     resolve(openapi as OpenAPIObject);
+  //   });
+  // });
 };
