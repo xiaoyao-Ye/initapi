36e4efb2249288d1ca453bf9bd9d9c063aae02e3
Ghosteye <yqcyecong@163.com>
2023-08-23 11:11:36 +0800

    perf: test

--- src/generateTS/schemaToType.spec.ts
+++ src/generateTS/schemaToType.spec.ts
@@ -28,26 +28,44 @@ describe("schema to type", () => {
     expect(res).toBe(undefined);
   });
 
-  it("should return type", () => {
-    expect(schemaToType({ type: "null" })).toBe("any");
-    expect(schemaToType({ type: "string" })).toBe("string");
-    expect(schemaToType({ type: "boolean" })).toBe("boolean");
-    expect(schemaToType({ type: "number" })).toBe("number");
-    expect(schemaToType({ type: "integer" })).toBe("number");
+  // it("should return type", () => {
+  //   expect(schemaToType({ type: "null" })).toBe("any");
+  //   expect(schemaToType({ type: "string" })).toBe("string");
+  //   expect(schemaToType({ type: "boolean" })).toBe("boolean");
+  //   expect(schemaToType({ type: "number" })).toBe("number");
+  //   expect(schemaToType({ type: "integer" })).toBe("number");
 
-    const arr: Schema = { type: "array", items: { type: "string" } };
-    expect(schemaToType(arr)).toBe("Array<string>");
+  //   const arr: Schema = { type: "array", items: { type: "string" } };
+  //   expect(schemaToType(arr)).toBe("Array<string>");
 
-    const arrRef: Schema = { type: "array", items: { $ref: "#/components/schemas/AdvertisementParam" } };
-    expect(schemaToType(arrRef)).toBe("Array<AdvertisementParam>");
+  //   const arrRef: Schema = { type: "array", items: { $ref: "#/components/schemas/AdvertisementParam" } };
+  //   expect(schemaToType(arrRef)).toBe("Array<AdvertisementParam>");
 
-    const obj: Schema = { type: "object" };
-    expect(schemaToType(obj)).toBe("Record<string, any>");
+  //   const obj: Schema = { type: "object" };
+  //   expect(schemaToType(obj)).toBe("Record<string, any>");
 
-    const objRef: Schema = { type: "object", additionalProperties: { $ref: "#/components/schemas/AdvertisementParam" } };
-    expect(schemaToType(objRef)).toBe("Record<string, AdvertisementParam>");
+  //   const objRef: Schema = { type: "object", additionalProperties: { $ref: "#/components/schemas/AdvertisementParam" } };
+  //   expect(schemaToType(objRef)).toBe("Record<string, AdvertisementParam>");
 
-    const objStr: Schema = { type: "object", additionalProperties: { type: "string" } };
-    expect(schemaToType(objStr)).toBe("Record<string, string>");
+  //   const objStr: Schema = { type: "object", additionalProperties: { type: "string" } };
+  //   expect(schemaToType(objStr)).toBe("Record<string, string>");
+  // });
+
+  it.each([
+    [{ type: "null" }, "any"],
+    [{ type: "string" }, "string"],
+    [{ type: "boolean" }, "boolean"],
+    [{ type: "number" }, "number"],
+    [{ type: "integer" }, "number"],
+    [{ type: "array", items: { type: "string" } }, "Array<string>"],
+    [{ type: "array", items: { $ref: "#/components/schemas/AdvertisementParam" } }, "Array<AdvertisementParam>"],
+    [{ type: "object" }, "Record<string, any>"],
+    [
+      { type: "object", additionalProperties: { $ref: "#/components/schemas/AdvertisementParam" } },
+      "Record<string, AdvertisementParam>",
+    ],
+    [{ type: "object", additionalProperties: { type: "string" } }, "Record<string, string>"],
+  ])("schemaToType(%s) should return %s", (schema, expected) => {
+    expect(schemaToType(schema as Schema)).toBe(expected);
   });
 });
