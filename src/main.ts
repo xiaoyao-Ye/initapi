e271fd1c7b908e2fd3ebd7b02aed3b0a54801945
_Ghosteye <yqcyecong@163.com>
2024-01-04 11:47:49 +0800

    chore: collation

--- src/main.ts
+++ src/main.ts
@@ -51,41 +51,43 @@ export const main = async () => {
     const dtoNameList = [...interfaceNameList, ...enumNameList];
     const { controllerList, importAllType } = generateAPI_TS(apiMap, { commonPrefix, multipleFiles, dtoNameList });
 
+    const filePath = `${serviceName}/typings.d.${fileSuffix}`;
     const renderOptionsType = { definition, enumMode, indexableTemplate, generateInterfaceList, generateEnumList };
     const ejs_template = resolve(__dirname, "./template/typeScript/typings.d.ejs");
     const template_typings = await ejsRender(ejs_template, renderOptionsType);
-    const filePath = `${serviceName}/typings.d.${fileSuffix}`;
     outputFile(outputDir, filePath, template_typings, prettierOptions);
 
     if (multipleFiles) {
       controllerList.forEach(async ({ description, controllerName, funcList, importType }) => {
+        const filePath = `${serviceName}/${controllerName}.${fileSuffix}`;
         const renderOptionsApi = { description, funcList, importType, importRequest, useRequest };
         const ejs_template = resolve(__dirname, "./template/typeScript/apiFiles.ejs");
         const template_API = await ejsRender(ejs_template, renderOptionsApi);
-        const filePath = `${serviceName}/${controllerName}.${fileSuffix}`;
         outputFile(outputDir, filePath, template_API, prettierOptions);
       });
     } else {
+      const filePath = `${serviceName}/api.${fileSuffix}`;
       const renderOptionsApi = { controllerList, importAllType, importRequest, useRequest };
       const ejs_template = resolve(__dirname, "./template/typeScript/api.ejs");
       const template_Api = await ejsRender(ejs_template, renderOptionsApi);
-      const filePath = `${serviceName}/api.${fileSuffix}`;
       outputFile(outputDir, filePath, template_Api, prettierOptions);
     }
   } else {
     const { controllerList } = generateAPI_JS(apiMap, { commonPrefix });
     if (multipleFiles) {
       controllerList.forEach(async ({ description, controllerName, funcList }) => {
+        const filePath = `${serviceName}/${controllerName}.${fileSuffix}`;
         const renderOptionsApi = { description, funcList, importRequest, useRequest };
         const ejs_template = resolve(__dirname, "./template/javaScript/apiFiles.ejs");
         const template_Api = await ejsRender(ejs_template, renderOptionsApi);
-        outputFile(outputDir, `${serviceName}/${controllerName}.${fileSuffix}`, template_Api, prettierOptions);
+        outputFile(outputDir, filePath, template_Api, prettierOptions);
       });
     } else {
+      const filePath = `${serviceName}/api.${fileSuffix}`;
       const renderOptionsApi = { controllerList, importRequest, useRequest };
       const ejs_template = resolve(__dirname, "./template/javaScript/api.ejs");
       const template_Api = await ejsRender(ejs_template, renderOptionsApi);
-      outputFile(outputDir, `${serviceName}/api.${fileSuffix}`, template_Api, prettierOptions);
+      outputFile(outputDir, filePath, template_Api, prettierOptions);
     }
   }
 
